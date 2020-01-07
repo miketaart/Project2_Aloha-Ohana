b03c8db565b654faa60a8c1ed06a8b6b1d71939a
Mike Ta <h.michael.ta@gmail.com>
2020-01-07 17:55:01 +0100

    refactoring

--- models/Tourist.js
+++ models/Tourist.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose")
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 Tourist = mongoose.model("tourists", {
-        purchased_tours: {type: ObjectId, ref: "Tour"},
-
+        //purchased_tours: {type: ObjectId, ref: "Tour"},
+        trigger: {type: Number},
+        purchased_tours: [{type: ObjectId, ref: "tours"}]
+        
     })
 
 module.exports = Tourist
--- routes/tourist/tours.js
+++ routes/tourist/tours.js
@@ -19,6 +19,7 @@ app.get("/list", (req,res, next)=> {
     Tour.find({})
     .then((tours)=> {
         res.render("tours/tours.hbs", {tours:tours});
+
     })
     .catch((err)=> {
         console.log("Err", err);
@@ -26,7 +27,68 @@ app.get("/list", (req,res, next)=> {
     })
 })
 
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// find by document id and update and push item in array
+/*users.findByIdAndUpdate(userID,
+    {$push: {friends: friend}},
+    {safe: true, upsert: true},
+    function(err, doc) {
+        if(err){
+        console.log(err);
+        }else{
+        //do stuff
+        }
+    }
+); */
+
+/*
+//POST method 
+app.post("/detail", (req, res) => {
+    let tourId = req.params.id
+    let purchasedTour = {
+        purchased_tours: req.body.purchased_tours
+    }
+
+    Tourist.findByIdAndUpdate(tourId, purchasedTour, {new:true})
+    .then((newTour) => {
+        res.redirect(`/tourist/tours/detail?id=${newTour.id}`)
+    })
+    .catch(err => console.log(err))
+})
+*/
+
+app.get("/add", (req,res, next)=> {
+    Tourist.find({})
+    .then((tours)=> {
+        res.render("tours/tours.hbs", {tours:tours});
+
+    })
+    .catch((err)=> {
+        console.log("Err", err);
+        next(createError(500, 'Sorry, our database crashed. Please come back later.'))
+    })
+})
+
+app.post("/add", (req, res) => {
+    let tourId = req.params.id;
+    let touristId = req.Tourist._id;
+    let newTourist = {
+        trigger: req.body.trigger,
+        purchased_tours: req.body.purchased_tours
+    }
+
+    Tourist.create(newTourist)
+    .then(() => {
+        res.redirect("/tourist/tours/list");
+    })
+    .catch(err => console.log(err))
+
+    Tourist.findByIdAndUpdate(touristId).push(purchased_tours: Tour.id}})
+    .then(() => {
+        res.redirect("/tourist/tours/list")
+    })
+})
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
